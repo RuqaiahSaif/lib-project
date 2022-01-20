cff0687577c71480fad06ef48d321146f9ffce21
RuqaiahSaif <ruqaiah.saif@gmail.com>
2022-01-20 15:48:12 +0300

    fifth

--- js/app2.js
+++ js/app2.js
@@ -38,3 +38,18 @@ function showSlides(n) {
     dots[slideIndex - 1].className += " active";
     captionText.innerHTML = dots[slideIndex - 1].alt;
 }
+/////////////// search
+function search_animal() {
+    let input = document.getElementById('searchbar').value
+    input = input.toLowerCase();
+    let x = document.getElementsByClassName('card');
+
+    for (i = 0; i < x.length; i++) {
+        if (!x[i].innerHTML.toLowerCase().includes(input)) {
+            x[i].style.display = "none";
+        }
+        else {
+            x[i].style.display = "list-item";
+        }
+    }
+}
